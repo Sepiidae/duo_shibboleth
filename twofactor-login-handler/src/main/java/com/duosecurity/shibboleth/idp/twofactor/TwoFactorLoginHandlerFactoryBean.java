28ab6a935285bd64dd322e6dc4ffc62d4681ab06
Rhian Resnick <rresnick@whitedeath.fau.edu>
2014-03-19 20:59:16 -0400

    This got missed in the commit

--- twofactor-login-handler/src/main/java/com/duosecurity/shibboleth/idp/twofactor/TwoFactorLoginHandlerFactoryBean.java
+++ twofactor-login-handler/src/main/java/com/duosecurity/shibboleth/idp/twofactor/TwoFactorLoginHandlerFactoryBean.java
@@ -27,6 +27,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * Rhian
  */
 package com.duosecurity.shibboleth.idp.twofactor;
 
